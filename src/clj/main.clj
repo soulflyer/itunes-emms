58773768e976c762eae83dc7edc3c3ab9526c3c9
iain <iain@soulflyer.co.uk>
2024-07-08 01:18:41 +0700

    Create map and output to file.

--- src/clj/main.clj
+++ src/clj/main.clj
@@ -2,6 +2,7 @@
   (:require
     [clojure.java.io :as io]
     [clojure.string :as s]
+    [clojure.pprint :as pp]
     [com.github.bdesham.clj-plist :as plist]
     [com.rpl.specter :refer [ALL MAP-KEYS MAP-VALS submap transform select]])
   (:import
@@ -15,38 +16,46 @@
   [url]
   (URLDecoder/decode url "UTF-8"))
 
-(defn all-artists
-  [tracks]
-  ["Moving Hearts" "Timo Maas"])
-
 (defn all-artist-tracks
   [tracks artist]
   {artist (group-by :album (get (group-by :artist tracks) artist))})
 
-(defn track-vector
+(defn remove-artist-and-album
+  [track-map]
+  (apply dissoc track-map [:artist :album]))
+
+(defn track-map
   ([filename]
-   (track-vector filename [:artist :album :name :location :rating :play-count :comments]))
+   (track-map filename [:name :location :rating :play-count :comments]))
   ([filename only]
    (let [file (java.io.File. filename)
-         tracks  (vec (vals (get (plist/parse-plist file) "Tracks")))
-         track-vector (->>
-                        tracks
-                        (transform [ALL MAP-KEYS] key->keyword)
-                        (transform [ALL (submap [:location]) MAP-VALS] url->string)
-                        (select [ALL (submap only)]))]
-     (first (map #(all-artist-tracks track-vector %)
-           (all-artists track-vector))))))
+         tracks           (vec (vals (get (plist/parse-plist file) "Tracks")))
+         only             (into only [:album :artist])
+         track-vector     (->>
+                            tracks
+                            (transform [ALL MAP-KEYS] key->keyword)
+                            (transform [ALL (submap [:location]) MAP-VALS] url->string)
+                            (select [ALL (submap only)]))
+         artists          (into [] (set (select [ALL :artist] track-vector)))]
+     (transform [MAP-VALS MAP-VALS ALL]
+                remove-artist-and-album
+                (into {}
+                      (map #(all-artist-tracks track-vector %)
+                           artists))))))
+;;TODO blank out the empty ones (albums and artists)
 
 (comment
-  (track-vector "sample.xml")
+  (track-map "sample.xml")
+  (track-map "Library.xml" [:comments])
+  (spit "/tmp/out.edn" (with-out-str (pp/pprint (track-map "Library.xml"))))
   )
-;; TODO Sort the vector by Artist, Album and Track-number
-;; TODO rename track-vector, its a map now
+
+;; TODO This may not work since track-map changed
 (defn plist->edn
   [in-file out-file & [only]]
   (let [tracks (if only
-                 (track-vector in-file only)
-                 (track-vector in-file))]
+                 (track-map in-file only)
+                 (track-map in-file))]
     (with-open [wtr (io/writer out-file)]
       (binding [*out* wtr]
         (println "[")
@@ -60,24 +69,21 @@
 
 (comment
   (plist->edn "sample.xml" "/tmp/plist.edn")
-  (track-vector "sample.xml" [:location])
-  (track-vector "sample.xml" [:name :artist])
-  (track-vector "sample.xml")
-  (group-by :album (track-vector "sample.xml"))
-  (group-by :artist (track-vector "sample.xml"))
+  (track-map "sample.xml" [:location])
+  (track-map "sample.xml" [:name :artist])
+  (track-map "sample.xml")
+  (keys (track-map "sample.xml"))
+  (group-by :album (track-map "sample.xml"))
+  (group-by :artist (track-map "sample.xml"))
 
-  (all-artists (track-vector "sample.xml"))
+  (group-by :album (get (group-by :artist (track-map "sample.xml")) "Moving Hearts"))
+  (all-artist-tracks (track-map "sample.xml") "Moving Hearts")
   
-  (group-by :album (get (group-by :artist (track-vector "sample.xml")) "Moving Hearts"))
-  (all-artist-tracks (track-vector "sample.xml") "Moving Hearts")
-;; TODO map this^ across all artists
-  (map #(all-artist-tracks (track-vector "sample.xml") %)
-       (all-artists (track-vector "sample.xml")))
 ;; TODO now get rid of the extra info in each track (artist and album)
   
-  (select [ALL (submap ["Location" "Play Count"])] (track-vector "sample.xml"))
-  (transform [ALL MAP-KEYS] key->keyword (track-vector "sample.xml"))
-  (->> (track-vector "sample.xml")
+  (select [ALL (submap ["Location" "Play Count"])] (track-map "sample.xml"))
+  (transform [ALL MAP-KEYS] key->keyword (track-map "sample.xml"))
+  (->> (track-map "sample.xml")
        (transform [ALL MAP-KEYS] key->keyword)
        (transform [ALL (submap [:location]) MAP-VALS] url->string))
   (url->string "file:///Users/iain/Music/Collection/TimoMaas/Loud/10%20To%20Get%20Down.mp3"))
